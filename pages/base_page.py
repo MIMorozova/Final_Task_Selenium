082063da33822b93544fd1ec91c3a8eda84794e2
MIMorozova <83887934+MIMorozova@users.noreply.github.com>
2021-05-15 18:01:33 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,11 @@
 class BasePage():
     def __init__(self, browser,url):
+        #добавим конструктор — метод, который вызывается, когда мы создаем объект
+        #качестве параметров мы передаем экземпляр драйвера и url адрес
         self.browser = browser
         self.url = url
 
     def open(self):
+        #должен открывать нужную страницу в браузере, используя метод get()
         self.browser.get(self.url)
 
