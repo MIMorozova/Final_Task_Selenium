66cdddaa573a3997e2353781101610a4258de1da
MIMorozova <83887934+MIMorozova@users.noreply.github.com>
2021-05-16 23:13:54 +0300

    Update locators.py
    добавлены локаторы для login_form и register_form

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,11 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    
+class LoginPageLocators():
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_LOGIN = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "[value='Register']")
