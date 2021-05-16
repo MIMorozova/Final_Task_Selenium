5e981c7c30e39ee17ebd87f81987ae46ef9115df
MIMorozova <83887934+MIMorozova@users.noreply.github.com>
2021-05-16 16:00:22 +0300

    Update base_page.py
    1. В конструктор BasePage добавим команду для неявного ожидания со значением по умолчанию в 10
    2. Теперь в этом же классе реализуем метод is_element_present, в котором будем перехватывать исключение. В него будем передавать два аргумента: как искать (css, id, xpath и тд) и собственно что искать (строку-селектор). 
    
    Чтобы перехватывать исключение, нужна конструкция try/except

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,23 @@
 #базовая страница, от которой будут унаследованы все остальные классы. В ней описываются вспомогательные методы для работы с драйвером.
 
+#from .locators import BaseLocators
+from selenium.common.exceptions import NoSuchElementException
+
 
 class BasePage():  #конструктор класса
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self): #открытие нужной страницы в браузере
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
 
